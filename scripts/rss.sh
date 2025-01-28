8a574a2f5facbf07107b6eb46942fd2d75dfe6d4
austinjp <austin.plunkett@gmail.com>
2025-01-28 10:25:34 +0000

    More RSS fiddling
    Correct date format (RFC822) and various locations for feed files.

--- scripts/rss.sh
+++ scripts/rss.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Kudos: https://recursewithless.net/projects/pandoc-feeds.html
+# Decisions about file names kudos: https://blog.jim-nielsen.com/2021/feed-urls/
+# Definitive RSS location:  /feed.xml
+# Definitive Atom location: /feed.atom
 
 set -euo pipefail
 
@@ -9,15 +12,34 @@ wd=$(readlink -qe "${this_dir}"/../)
 
 cd "${wd}"
 
+f="feed.xml"
+
+# Remember, date must be in RFC822 format.
+
 pandoc \
-    -M updated="$(date '+%Y-%m-%d')" \
+    -M updated="$(date '+%a, %d %b %Y %H:%M:%S %Z')" \
     -M title='Austin Plunkett' \
     --metadata-file=feed.yaml \
     --template=templates/rss.xml \
     --wrap=none \
     -t html \
     < /dev/null \
-    | sed -re 's/^\s+//g' \
-    | grep -v '^$' \
-    | tr -d $'\n' \
-         > rss.xml
+    > "${f}"
+
+# Minify pandoc's output:
+#     | sed -re 's/^\s+//g' \
+#     | grep -v '^$' \
+#     | tr -d $'\n' \
+
+
+# Remember to write over file $f last.
+for t in "feed.atom" "${f}" ; do
+    tmpfile=$(mktemp)
+    unset FILENAME 2>/dev/null || :
+    FILENAME="${t}" envsubst < <(cat "${f}") > "${tmpfile}"
+    cp -f "${tmpfile}" "${t}"
+    rm -f "${tmpfile}"
+done
+
+# Hmm skip Atom for now! (:
+rm -r feed.atom 2>/dev/null || :
